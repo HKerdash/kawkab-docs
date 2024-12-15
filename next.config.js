009aeb3505f0263fcc007060969eda461370ea07
Hassan Kerdash <52297419+HKerdash@users.noreply.github.com>
2024-12-15 04:49:41 +0200

    edit next.config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,13 @@
 import nextra from 'nextra'
  
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true // mandatory, otherwise won't export
-  }
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: "build"
-  ,
-  i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en'
-  }
-}
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx'
 })
  
-export default withNextra(nextConfig)
+export default withNextra({
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en'
+  }
+})
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { useRouter } from "next/router";
 
 export default {
   logo: <span>Kawkab JS</span>,
   project: {
     link: "https://github.com/kawkabjs/docs",
-  },
-  useNextSeoProps() {
-    const { asPath } = useRouter()
-    if (asPath !== '/') {
-      return {
-        titleTemplate: '%s – Nextra'
-      }
-    }
   },
   footer: {
     content: (
@@ -27,6 +18,6 @@ export default {
   },
   i18n: [
     { locale: 'en', name: 'English' },
-    { locale: 'ar', name: 'العربية', direction: 'rtl' }
+    { locale: 'ar', name: 'العربية', direction: 'rtl' },
   ]
 };
